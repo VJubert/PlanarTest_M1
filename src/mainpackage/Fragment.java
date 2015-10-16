11cf6a81faf39ebcbf1e214efe369a90f900d259
Valball <val_jub@live.fr>
2015-10-16 17:50:03 +0200

    squelette

---
+++ src/mainpackage/Fragment.java
@@ -0,0 +1,15 @@
+package mainpackage;
+
+public class Fragment {
+
+	public boolean hasFaceAdmissible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean hasOneFace() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/mainpackage/Main.java
@@ -0,0 +1,82 @@
+package mainpackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	private static Graphe g;
+	private static List<Fragment> list_frag=new ArrayList<Fragment>();
+
+	public static void main(String[] args) {
+		file_to_graph(args[0]);
+		calcul_cycle(g);
+		calcul_face();
+		boolean une_seul_face=false;
+		while(has_frag(g)){
+			calcul_frag(g);
+			une_seul_face=false;
+			for (Fragment frag : list_frag) {
+				if(!une_seul_face){
+					calcul_face_admissible(frag);
+					if(!frag.hasFaceAdmissible()){
+						System.out.println("false");
+						System.exit(0);
+					}
+					else {
+						if(frag.hasOneFace()){
+							plonger(frag);
+							une_seul_face=true;
+						}
+					}
+				}
+			}
+			if(!une_seul_face){
+				plonger(list_frag.get(0));
+			}
+		}
+		System.out.println("true");
+		afficherCartePlanaire();
+	}
+
+	private static void afficherCartePlanaire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void plonger(Fragment frag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void calcul_face_admissible(Fragment frag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void calcul_frag(Graphe g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static boolean has_frag(Graphe g2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static void calcul_face() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void calcul_cycle(Graphe g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void file_to_graph(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
