cae157fe105e173015259d6fcab33dec83b71a2f
Valball <val_jub@live.fr>
2015-11-17 20:33:12 +0100

    some function for frag

--- src/mainpackage/Fragment.java
+++ src/mainpackage/Fragment.java
@@ -1,15 +1,38 @@
 package mainpackage;
 
-public class Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-	public boolean hasFaceAdmissible() {
-		// TODO Auto-generated method stub
-		return false;
+public class Fragment {
+	
+	private List<Sommet> sommets;
+	private List<Face> face;
+	
+	public Fragment(List<Sommet> sommets) {
+		this.sommets = sommets;
+		face=new ArrayList<Face>();
 	}
 
-	public boolean hasOneFace() {
-		// TODO Auto-generated method stub
-		return false;
+	public List<Sommet> getSommets() {
+		return sommets;
+	}
+
+	public List<Face> getFace() {
+		return face;
+	}
+
+	public int calcul_face_admissible(List<Face> list_face) {
+		Sommet s=sommets.get(0);
+		Sommet s2=sommets.get(sommets.size()-1);
+		ArrayList<Sommet> a=new ArrayList<Sommet>();
+		a.add(s);
+		a.add(s2);
+		for (Face f : list_face) {
+			if(f.getSommets().containsAll(a)){
+				face.add(f);
+			}
+		}
+		return face.size();
 	}
 
 }
--- src/mainpackage/Main.java
+++ src/mainpackage/Main.java
@@ -7,43 +7,44 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	
-	private static Graphe g;
-	private static List<Fragment> list_frag=new ArrayList<Fragment>();
-	private static List<Face> list_face=new ArrayList<Face>();
 
-	public static void main(String[] args) {		
+	private static Graphe g;
+	private static List<Fragment> list_frag = new ArrayList<Fragment>();
+	private static List<Face> list_face = new ArrayList<Face>();
+
+	public static void main(String[] args) {
 		file_to_graph(args[0]);
-		if(!g.calculCycle(g.getPremierSommet())){
+		if (!g.calculCycle(g.getPremierSommet())) {
 			System.out.println("true");
 			System.out.println("Pas de cycle => Arbre => Toujours planaire");
-			return ;
-		}else{//il y a un cycle, je l'affiche
+			return;
+		} else {// il y a un cycle, je l'affiche
 			g.majmarquage();
 			System.out.println("il y a un cycle !");
 			System.out.println(g.toStringCycle());
 		}
 		init_face();
-		boolean une_seul_face=false;
-		while(g.has_frag()){
+		boolean une_seul_face = false;
+		while (g.has_frag()) {
 			calcul_frag(g);
-			une_seul_face=false;
+			une_seul_face = false;
 			for (Fragment frag : list_frag) {
-				if(!une_seul_face){
-					calcul_face_admissible(frag);
-					if(!frag.hasFaceAdmissible()){
-						System.out.println("false");
+				if (!une_seul_face) {
+					int nb_face_admissible = frag.calcul_face_admissible(list_face);
+					switch (nb_face_admissible) {
+					case 0:
+						System.out.println(false);
 						return;
+					case 1:
+						plonger(frag);
+						une_seul_face = true;
+						break;
 					}
-					else {
-						if(frag.hasOneFace()){
-							plonger(frag);
-							une_seul_face=true;
-						}
-					}
+				} else {
+					break;
 				}
 			}
-			if(!une_seul_face){
+			if (!une_seul_face) {
 				plonger(list_frag.get(0));
 			}
 		}
@@ -54,32 +55,32 @@ public class Main {
 	private static void init_face() {
 		list_face.clear();
 		list_face.add(new Face(g.getCycle()));
-		list_face.add(new Face(g.getCycle()));		
+		list_face.add(new Face(g.getCycle()));
 	}
 
 	private static void afficherCartePlanaire() {
 		list_face.forEach(System.out::println);
-		
+
 	}
 
 	private static void plonger(Fragment frag) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void calcul_face_admissible(Fragment frag) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void calcul_frag(Graphe g2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void calcul_face() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void file_to_graph(String fileName) {
@@ -90,11 +91,11 @@ public class Main {
 		numLigne = 1;
 		try {
 			fileScanner = new Scanner(new File(fileName));
-			while(fileScanner.hasNextLine()) {
+			while (fileScanner.hasNextLine()) {
 				ligne = fileScanner.nextLine();
-				if(numLigne == 1){
+				if (numLigne == 1) {
 					nbSommets = Integer.parseInt(ligne);
-					g=new Graphe(nbSommets);
+					g = new Graphe(nbSommets);
 				} else {
 					// Récupération du numéro du sommet.
 					lineScanner = new Scanner(ligne);
@@ -102,11 +103,11 @@ public class Main {
 					numSommet = Integer.parseInt(lineScanner.next());
 					// Récupération de l'ensemble des voisins du sommet.
 					// L'ensemble récupéré est de la forme "1, 2, 3".
-					voisins = ligne.substring(4+(numLigne/12), ligne.length()-1);
+					voisins = ligne.substring(4 + (numLigne / 12), ligne.length() - 1);
 					lineScanner = new Scanner(voisins);
 					lineScanner.useDelimiter(", ");
 					tableauVoisins = new ArrayList<Integer>();
-					while(lineScanner.hasNext()) {
+					while (lineScanner.hasNext()) {
 						numVoisin = Integer.parseInt(lineScanner.next());
 						// On ajoute numVoisin dans tableauVoisins.
 						tableauVoisins.add(Integer.valueOf(numVoisin));
@@ -119,6 +120,7 @@ public class Main {
 				}
 				numLigne++;
 			}
-		} catch(IOException ioException) {}
+		} catch (IOException ioException) {
+		}
 	}
 }
